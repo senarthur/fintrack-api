66ce76ceaf66e734a8b8e24f535c9a79e082a7e9
Arthur Sena <sena.arthur698@gmail.com>
2026-03-18 09:33:35 -0300

    Fix(controller): change updateTransaction method from POST to PUT

--- src/main/java/com/arthursena/fin_track/controller/TransactionController.java
+++ src/main/java/com/arthursena/fin_track/controller/TransactionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +47,7 @@ public class TransactionController {
         return ResponseEntity.ok(newTransaction);
     }
     
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Transaction> updateTransaction(@PathVariable String id, @RequestBody Transaction updatedTransaction) {
         Transaction transaction = transactionService.updateTransaction(id, updatedTransaction);
         return transaction != null ? ResponseEntity.ok(transaction) : ResponseEntity.notFound().build();
