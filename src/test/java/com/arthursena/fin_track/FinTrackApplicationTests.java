a53484dbb03a9833bc4367f8584555a0aee9b2ab
Arthur Sena <sena.arthur698@gmail.com>
2026-03-18 09:18:39 -0300

    Initial commit

---
+++ src/test/java/com/arthursena/fin_track/FinTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthursena.fin_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
