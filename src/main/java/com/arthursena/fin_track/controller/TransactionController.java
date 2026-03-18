27dbfeb6e6ec418a2891d69e7b004c47ad027ac8
Arthur Sena <sena.arthur698@gmail.com>
2026-03-18 09:25:15 -0300

    feat(controller): create transaction controller

---
+++ src/main/java/com/arthursena/fin_track/controller/TransactionController.java
@@ -0,0 +1,67 @@
+package com.arthursena.fin_track.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arthursena.fin_track.model.Transaction;
+import com.arthursena.fin_track.service.TransactionService;
+
+
+@RestController
+@RequestMapping("/api/transactions")
+@CrossOrigin(origins = "http://localhost:4200")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.findAllTransactions());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable String id) {
+        return transactionService.findTransactionById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
+        Transaction newTransaction = transactionService.saveTransaction(transaction);
+        return ResponseEntity.ok(newTransaction);
+    }
+    
+    @PostMapping("/{id}")
+    public ResponseEntity<Transaction> updateTransaction(@PathVariable String id, @RequestBody Transaction updatedTransaction) {
+        Transaction transaction = transactionService.updateTransaction(id, updatedTransaction);
+        return transaction != null ? ResponseEntity.ok(transaction) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable String id) {
+        transactionService.deleteTransaction(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<BigDecimal> getBalance() {
+        BigDecimal balance = transactionService.calculateBalance();
+        return ResponseEntity.ok(balance);
+    }
+    
+} 
